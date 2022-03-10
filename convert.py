a6c45b5faa8d8b3baaa029ba8753b02dc59ac6b4
Stephen Grice <stephengrice@live.com>
2022-03-10 12:27:44 -0800

    Specify encoding for intermediate file

--- convert.py
+++ convert.py
@@ -37,7 +37,7 @@ def fix_image_names(md_path, image_names):
     tmp_path = md_path + '.tmp'
     i = 0
     with open(md_path, 'r', encoding='utf-8') as f_md:
-        with open(tmp_path, 'w') as f_tmp:
+        with open(tmp_path, 'w', encoding='utf-8') as f_tmp:
             body_md = f_md.read()
             for i,name in enumerate(image_names):
                 # TODO assumes all are png
